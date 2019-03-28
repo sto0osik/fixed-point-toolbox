06ee89f46bf22d9d53eb697611d50632ab8a8ffe
Łukasz Tomaszewski <lukasztomaszewski@ukaszs-MacBook-Air.local>
2019-03-28 12:43:30 +0100

    Please enter the commit message for your changes. Lines starting Error when trying to add two negative values#

--- fixedpointbox.py
+++ fixedpointbox.py
@@ -3,6 +3,7 @@ import numpy as np
 
 class FixedNum():
     def __init__(self, value, word_length, frac_bits):
+        np.seterr(all='ignore')
         self.word_length = word_length
         self.frac_bits = frac_bits
         self.value = self.assign_type(value)
@@ -14,10 +15,18 @@ class FixedNum():
                 f'Fractional bits: {self.frac_bits}')
 
     def __add__(self, other):
-        return FixedNum(self.value + other.value, self.word_length, self.frac_bits)
+        if (self.value > 0 and other.value > 0) and (self.value + other.value < 0):
+            return FixedNum(np.iinfo(type(self.value)).max, self.word_length, self.frac_bits)
+        elif (self.value < 0 and other.value < 0) and (self.value + other.value > 0):
+            return FixedNum(np.iinfo(type(self.value)).min, self.word_length, self.frac_bits)
+        else:
+            return FixedNum(self.value + other.value, self.word_length, self.frac_bits)
 
     def __sub__(self, other):
         return FixedNum(self.value - other.value, self.word_length, self.frac_bits)
+
+    def __mul__(self, other):
+        return FixedNum(self.value * other.value, self.word_length, self.frac_bits)
 
     def assign_type(self, value):
         if self.word_length == 8:
@@ -32,14 +41,21 @@ class FixedNum():
 
 
 if __name__ == '__main__':
-    x = FixedNum(32000, 16, 1)
-    y = FixedNum(32000, 16, 1)
+    x = FixedNum(-32000, 16, 0)
+    y = FixedNum(-32000, 16, 0)
+
     z = x + y
+
     print(z)
     print(type(z))
     print()
 
-    z = x - y
-    print(z)
-    print(type(z))
-    print()
+    # z = x - y
+    # print(z)
+    # print(type(z))
+    # print()
+    #
+    # z = x * y
+    # print(z)
+    # print(type(z))
+    # print()
